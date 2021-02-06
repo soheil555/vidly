b0c5e3f8fff155c3c37b2eafe3ddfacfaee66219
soheil rasekh <soheilrasekh555@gmail.com>
2021-02-06 13:30:58 +0330

    send jwt to client on authentication

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const users = require("./routes/users");
 const rentals = require("./routes/rentals");
+const auth = require("./routes/auth");
 
 //Enviroment Variables
 const port = process.env.PORT || 3000
@@ -33,6 +34,7 @@ app.use("/api/customers",customers);
 app.use("/api/movies",movies);
 app.use("/api/rentals",rentals);
 app.use("/api/users",users);
+app.use("/api/auth",auth);
 
 app.listen(port,()=>{
     console.log(`[+]Started at port ${port}`)
---
+++ routes/auth.js
@@ -0,0 +1,45 @@
+const {User} = require("../models/user");
+const express = require("express");
+const Joi = require("joi");
+const _ = require("loadsh");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+
+
+const router = express.Router();
+
+
+router.post("/",async (req,res) => {
+
+    const error = validateUser(req.body);
+    if(error) return res.send(error.details[0].message).status(400);
+
+    const user = await User.findOne({"email":req.body.email});
+    if(!user) return res.send("Invalid Email or Password.").status(400);
+
+    const passIsValid = await bcrypt.compare(req.body.password,user.password);
+    if(!passIsValid) return res.send("Invalid Email or Password.").status(400);
+
+    const token = jwt.sign({id:user._id},'secret');
+    res.send(token);
+
+});
+
+
+const userSchemaJoi = Joi.object({
+
+    email:Joi.string().email().required().min(5).max(100),
+    password:Joi.string().min(5).max(255).required()
+
+});
+
+
+function validateUser(user){
+    return userSchemaJoi.validate(user).error;
+}
+
+
+
+
+
+module.exports = router;
